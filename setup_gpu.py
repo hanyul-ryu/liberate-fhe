3f1b1c631c7d38de09b0e4e276277a20590a0493
hanyul-ryu <hanyul.ryu@desilo.ai>
2024-09-25 17:45:57 +0900

    merge gpu & cpu

--- setup_gpu.py
+++ setup_gpu.py
@@ -4,13 +4,49 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 path_source = "src/liberate/gpu"
+
+
+class CustomBuildExt(BuildExtension):
+    def build_extension(self, ext):
+        if ext.name == "ntt_cuda":
+            self.build_lib = os.path.join(path_source, "ntt")
+        else:
+            self.build_lib = os.path.join(path_source, "csprng")
+
+        os.environ["MAX_JOBS"] = str(os.cpu_count())
+        super().build_extension(ext)
+
+
 ext_modules = [
+    ###################
+    ####    ntt    ####
+    ###################
+    CUDAExtension(
+        name="ntt_cuda",
+        sources=[
+            os.path.join(path_source, "ntt/ntt.cpp"),
+            os.path.join(path_source, "ntt/ntt_cuda_kernel.cu"),
+        ],
+        extra_compile_args={
+            "cxx": [
+                "-O3", "-DNDEBUG"
+            ]
+        }
+    ),
+    ###################
+    ####    rng    ####
+    ###################
     CUDAExtension(
         name="randint_cuda",
         sources=[
             os.path.join(path_source, "csprng/randint.cpp"),
             os.path.join(path_source, "csprng/randint_cuda_kernel.cu"),
         ],
+        extra_compile_args={
+            "cxx": [
+                "-O3", "-DNDEBUG"
+            ]
+        }
     ),
     CUDAExtension(
         name="randround_cuda",
@@ -18,15 +54,23 @@ ext_modules = [
             os.path.join(path_source, "csprng/randround.cpp"),
             os.path.join(path_source, "csprng/randround_cuda_kernel.cu"),
         ],
+        extra_compile_args={
+            "cxx": [
+                "-O3", "-DNDEBUG"
+            ]
+        }
     ),
     CUDAExtension(
         name="discrete_gaussian_cuda",
         sources=[
             os.path.join(path_source, "csprng/discrete_gaussian.cpp"),
-            os.path.join(
-                path_source, "csprng/discrete_gaussian_cuda_kernel.cu"
-            ),
+            os.path.join(path_source, "csprng/discrete_gaussian_cuda_kernel.cu"),
         ],
+        extra_compile_args={
+            "cxx": [
+                "-O3", "-DNDEBUG"
+            ]
+        }
     ),
     CUDAExtension(
         name="chacha20_cuda",
@@ -34,40 +78,20 @@ ext_modules = [
             os.path.join(path_source, "csprng/chacha20.cpp"),
             os.path.join(path_source, "csprng/chacha20_cuda_kernel.cu"),
         ],
+        extra_compile_args={
+            "cxx": [
+                "-O3", "-DNDEBUG"
+            ]
+        }
     ),
-]
-
-ext_modules_ntt = [
-    CUDAExtension(
-        name="ntt_cuda",
-        sources=[
-            os.path.join(path_source, "ntt/ntt.cpp"),
-            os.path.join(path_source, "ntt/ntt_cuda_kernel.cu"),
-        ],
-    )
 ]
 
 if __name__ == "__main__":
     setup(
-        name="csprng",
+        name="extensions",
         ext_modules=ext_modules,
-        cmdclass={"build_ext": BuildExtension},
-        script_args=["build_ext"],
-        options={
-            "build": {
-                "build_lib": "src/liberate/csprng",
-            }
+        cmdclass={
+            "build_ext": CustomBuildExt
         },
-    )
-
-    setup(
-        name="ntt",
-        ext_modules=ext_modules_ntt,
-        script_args=["build_ext"],
-        cmdclass={"build_ext": BuildExtension},
-        options={
-            "build": {
-                "build_lib": "src/liberate/ntt",
-            }
-        },
+        script_args=["build_ext"]
     )
