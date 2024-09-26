0b85761c26e436eba617979c5ccbb51c1ab013a0
hanyul-ryu <hanyul.ryu@desilo.ai>
2024-09-26 14:12:01 +0900

    conditional build

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
-# build.py
 import subprocess
+import torch
 
-# Call setup_gpu.py to build CUDA extensions
-subprocess.check_call(["python", "setup_gpu.py", "build_ext", "--inplace"])
-
-# Call setup_cpu.py to build C++ extensions
-subprocess.check_call(["python", "setup_cpu.py", "build_ext", "--inplace"])
+if torch.cuda.is_available():
+    # Call setup_gpu.py to build CUDA extensions
+    subprocess.check_call(["python", "setup_gpu.py", "build_ext", "--inplace"])
+else:
+    # Call setup_cpu.py to build C++ extensions
+    subprocess.check_call(["python", "setup_cpu.py", "build_ext", "--inplace"])
